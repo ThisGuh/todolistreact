37808fba32df1eee99f06a4cf9f5c4e0bde284a5
ThisGuh <jakiepolska1111@gmail.com>
2019-11-18 18:59:09 +0100

    Updates

---
+++ precache-manifest.39b14d4713516ddcb4cc7a0ee0fd0ce9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eadb63e4c98cba43bc37d38613b525ad",
+    "url": "/todolistreact/index.html"
+  },
+  {
+    "revision": "353d1c4a598d2139d6a0",
+    "url": "/todolistreact/static/css/main.5035a488.chunk.css"
+  },
+  {
+    "revision": "e05867956baa26855fa3",
+    "url": "/todolistreact/static/js/2.d5336dad.chunk.js"
+  },
+  {
+    "revision": "353d1c4a598d2139d6a0",
+    "url": "/todolistreact/static/js/main.9faf2804.chunk.js"
+  },
+  {
+    "revision": "9d90c38a479c8e3e312c",
+    "url": "/todolistreact/static/js/runtime-main.92181ad8.js"
+  }
+]);
